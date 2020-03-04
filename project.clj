4260231f16e9c56927843dd90874496d1c474e59
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-03-04 17:58:32 +0100

    INIT

---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject integrant-tools "0.0.0"
+  :description "A library with helper functions, reader tags, and init-keys for Integrant"
+  :url "https://github.com/kwrooijen/integrant-tools"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[integrant "0.8.0"]]
+  :plugins [[lein-cloverage "1.0.13"]
+            [lein-shell "0.5.0"]
+            [lein-ancient "0.6.15"]
+            [lein-changelog "0.3.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+  :deploy-repositories [["releases" :clojars]]
+  :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[integrant-tools \"[0-9.]*\"\\\\]/[integrant-tools \"${:version}\"]/" "README.md"]}
+  :release-tasks [["shell" "git" "diff" "--exit-code"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["changelog" "release"]
+                  ["update-readme-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["vcs" "push"]])
