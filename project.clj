80fb08b375eb4f8023956497aa50db30f774310f
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-03-04 20:30:04 +0100

    Setup clojars

--- project.clj
+++ project.clj
@@ -9,7 +9,10 @@
             [lein-ancient "0.6.15"]
             [lein-changelog "0.3.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
-  :deploy-repositories [["releases" :clojars]]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[integrant-tools \"[0-9.]*\"\\\\]/[integrant-tools \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
                   ["change" "version" "leiningen.release/bump-version"]
